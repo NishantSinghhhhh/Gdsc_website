a255353afd3e75483c8b2c8461e25e03b1578a9a
NishantSinghhhhh <nishantsingh_230137@aitpune.edu.in>
2024-09-29 00:48:23 +0530

    changing-footer-01

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,29 +1,52 @@
 import React from 'react';
 import { ReactComponent as Hero1 } from '../Assets/Footer/hero-section 1.svg';
-import {ReactComponent as Logo} from '../Assets/Footer/Group 48096047.svg'
-
+import {ReactComponent as Logo} from '../Assets/Footer/gdsc.svg'
+import { FaInstagram, FaFacebook, FaLinkedin, FaTwitter } from 'react-icons/fa';
 const Footer = () => {
   return (
     <div className='bg-black h-[75vh] md:h-[95vh] p-[2.3rem] w-[99vw] '>
-      <div className='bg-[#E6E4D5] h-[50vh] rounded-[2rem] flex p-[3rem] '>
-        <div className='w-[30%]'>
+      <div className='bg-[#E6E4D5] h-[60vh] rounded-[2rem] flex p-[3rem] '>
+        <div className='w-[30%] flex flex-col gap-[25px]'>
             <div className='flex items-center'>
-            <Logo className='w-[100px] h-[100px] bg-blue-400' /> {/* Adjust width and height */}
-                <div className='flex flex-col ml-[10px]'>
-                <h4 className='text-xl'>Google Developer Student Clubs</h4> {/* Increased font size */}
-                <h5 className='text-lg'>Army Institute of Technology, Pune</h5> {/* Increased font size */}
+                <Logo className='w-[60px] h-[60px]' /> 
+                <div className='flex flex-col ml-[10px] '>
+                <h4 className='text-lg'>Google Developer Student Clubs</h4> 
+                <h5 className='text-sm'>Army Institute of Technology, Pune</h5> 
                 </div>
             </div>
-            
-            <div className='text-lg mt-[10px]'>medium</div> {/* Increase font size for medium */}
-            <div className='text-lg'>icons</div> {/* Increase font size for icons */}
+
+            <div className='text-[20px] font-medium mt-[10px] mb-[20px]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</div> 
+            <div className='text-xl flex space-x-4'>
+                <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+                    <FaInstagram className='text-[#E1306C] text-2xl w-[40px] h-[40px]' /> {/* Instagram color */}
+                </a>
+                <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+                    <FaTwitter className='text-[#1DA1F2] text-2xl w-[40px] h-[40px]' /> {/* Twitter color */}
+                </a>
+                <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+                    <FaLinkedin className='text-[#0077B5] text-2xl w-[40px] h-[40px]' /> {/* LinkedIn color */}
+                </a>
+                <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+                    <FaFacebook className='text-[#3B5998] text-2xl w-[40px] h-[40px]' /> {/* Facebook color */}
+                </a>
         </div>
 
+            </div>
 
-            <div className='flex w-[70%] bg-blue-400'>
-                <div>
-                    <div>1st</div>
-                    <div>1st Column</div>
+
+            <div className='flex w-[70%] bg-blue-400n '>
+                <div className=' flex flex-col gap-[40px]'>
+                    <div className='text-[40px] text-black font-bold'>Explore</div>
+                    
+                    <div cla>
+                    <ul className="list-disc flex flex-col gap-[10px] pl-5  ml-[25px] text-xl">
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Events</li>
+                        <li>Team</li>
+                    </ul>
+                </div>
+
                 </div>
 
                 <div>
