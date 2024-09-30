ca8514b71d251ffe4772dcf2e6c5e7459230318c
NishantSinghhhhh <nishantsingh_230137@aitpune.edu.in>
2024-10-01 00:49:15 +0530

    Event section nearly done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Collaborate from './sections/Collaborate';
+import Events from './sections/Events';
 import Footer from './sections/Footer';
 import Home from './sections/Home';
 import Team from './sections/Team';
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
     <Home/>
+    <Events/>
     <Team/>
     <Collaborate/>
     <Footer/>
